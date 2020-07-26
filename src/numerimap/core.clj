32f158d1668ee89827f09a0c86a1c9b29410bfcf
alphajuliet <andrew@alphajuliet.com>
2020-07-26 10:33:26 +1000

    Implement m-intersection. Implement m-dot, m-max, m-mul. Update unit tests.

--- src/numerimap/core.clj
+++ src/numerimap/core.clj
@@ -1,52 +1,47 @@
 ;; numerimap/core.clj
 ;; AndrewJ 2020-07-25
 
-(ns numerimap.core)
+(ns numerimap.core
+  (:require [clojure.set :as set]))
 
+;;-----------------------
 (defn m-apply
   "Apply a unary function to the values of the map."
   [f m]
   (apply f (vals m)))
 
 (def m-min (partial m-apply min))
+(def m-max (partial m-apply max))
 (def m-sum (partial m-apply +))
 
+;;-----------------------
 (def m-union merge-with)
+(defn m-intersection
+  "Return a map with pairwise functon f of the values of the intersection of keys in the two maps. This is the intersection version of `merge-with`."
+  [f m1 m2]
+  (into {}
+        (for [k (set/intersection (set (keys m1)) (set (keys m2)))]
+          {k (f (k m1) (k m2))})))
 
+;;-----------------------
 (def m-add (partial m-union +))
 (def m-sub (partial m-union -))
-;; (def m-mul (partial m-union *))
+(def m-mul (partial m-intersection *)) ;; pairwise (Hadamard) product
+(def m-dot (comp m-sum m-mul)) ;; dot product
 
+;;-----------------------
 (defn m-enumerate
   "For each pair [k v] in a numeric map, add v copies of k, and concatenate into a single list."
-  [h]
+  [m]
   (reduce-kv
    (fn [m k v]
-     (into m (repeat v k))) [] h))
+     (into m (repeat v k))) [] m))
 
+;;-----------------------
 (defn m-collect
   "Collect a list into a numeric map of counts."
   [lst]
   (into {} (map (fn [[k v]] [k (count v)])
                 (group-by identity lst))))
-
-; From Racket...
-;(defn map-intersection [f h1 h2]
-;         ;#:combine/key [combine/key (λ (k x y) (* x y))]
-;         ;h1 h2)
-;  (for/map ([k (in-list (intersection (map-keys h1) (map-keys h2)))])
-;            (values k (combine/key k (map-ref h1 k) (map-ref h2 k)))))
-
-;(define (map-union
-;         #:combine [combine #f]
-;         #:combine/key [combine/key
-;                        (if combine
-;                          (lambda (k x y) (combine x y))
-;                          (map-duplicate-error 'map-union))]
-;         one . rest)
-;  (for*/fold ([one one]) ([two (in-list rest)] [(k v) (in-map two)])
-;    (map-set one k (if (map-has-key? one k)
-;                        (combine/key k (map-ref one k) v)
-;                        v))))
 
 ;; The End
--- test/numerimap/core_test.clj
+++ test/numerimap/core_test.clj
@@ -6,6 +6,11 @@
   (testing "Sanity test."
     (is (= 5 (+ 2 3)))))
 
+(deftest intersection
+  (testing "Map intersection."
+    (let [m1 {:a 1 :b 2 :c 3}
+          m2 {:a 4 :c 5 :d 6}]
+      (is (= {:a 5 :c 8} (num/m-intersection + m1 m2))))))
 (deftest num-ops
   (testing "Unary operations"
     (is (= 1 (num/m-min {:a 1 :b 2})))
@@ -13,16 +18,22 @@
     (is (= [:a :b :b :c :c :c]
            (num/m-enumerate {:a 1 :b 2 :c 3}))))
 
-  (testing "Add"
-    (is (= {:a 4 :b 6}
-           (num/m-add {:a 1 :b 2} {:a 3 :b 4})))
-    (is (= {:a 9 :b 12}
-           (num/m-add {:a 1 :b 2} {:a 3 :b 4} {:a 5 :b 6})))
-    (is (= {:a 4 :b 6 :c 1}
-           (num/m-add {:a 1 :b 2} {:a 3 :b 4 :c 1}))))
+  (testing "Add/sub/mul"
+    (let [m1 {:a 1 :b 2}
+          m2 {:a 3 :b 4}
+          m3 {:a 5 :b 6}]
 
-  (testing "Subtract"
-    (is (= {:a 2 :b 2}
-           (num/m-sub {:a 3 :b 4} {:a 1 :b 2})))))
+      (is (= {:a 4 :b 6}
+             (num/m-add m1 m2)))
+      (is (= {:a 9 :b 12}
+             (num/m-add m1 m2 m3)))
+      (is (= {:a 4 :b 6 :c 1}
+             (num/m-add m1 {:a 3 :b 4 :c 1})))
+      (is (= {:a 2 :b 2}
+             (num/m-sub {:a 3 :b 4} {:a 1 :b 2})))
+      (is (= {:a 3 :b 8}
+             (num/m-mul m1 m2)))))
+
+  (testing "Subtract"))
 
 ;; The End
